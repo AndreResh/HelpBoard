54066a252878c4389b280516c02169a8bbc9fff0
reshe <reshetnyak.andrej2016@yandex.ru>
2024-06-19 20:56:47 +0300

    configService + swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { RequestModule } from './request/request.module';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UserModule} from './user/user.module';
+import {RequestModule} from './request/request.module';
+import {ConfigModule} from "@nestjs/config";
+import configurations from './configuration';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'andre',
-      password: 'andre',
-      database: 'andre',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
-    RequestModule,
-  ],
+    imports: [
+        ConfigModule.forRoot({isGlobal: true, load: [configurations]}),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'andre',
+            password: 'andre',
+            database: 'andre',
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+        UserModule,
+        RequestModule,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle("Help App")
+      .setDescription("BEST APP")
+      .setVersion("1.0")
+      .addTag('API')
+      .build();
+  const documentSwagger = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api', app, documentSwagger);
+
+  await app.listen(port);
 }
 bootstrap();
